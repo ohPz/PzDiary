e51e8a985d83cac3da8692fd7bce8215988e4f5b
laire7 <syoungkwak@gmail.com>
2024-06-20 15:45:28 +0900

    1input

---
+++ PzDiary/src/container/laire7.jsx
@@ -0,0 +1,9 @@
+export default function laire7() {
+  <div className="TopBottom" id="topHalf">
+  </div>
+  <div className="TopBottom" id="bottomHalf">
+    <span className="PlanDoProgress" id="planSpan"></span>
+    <span className="PlanDoProgress" id="doSpan"></span>
+    <span className="PlanDoProgress" id="progressSpan"></span>
+  </div>;
+}
