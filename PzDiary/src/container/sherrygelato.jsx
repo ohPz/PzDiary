83b041916110e00a2a9cc3a1a60cb7fc54196bd1
sherrygelato <sherrygelato@kakao.com>
2024-06-20 15:43:26 +0900

    Layout 연습 시작

---
+++ PzDiary/src/container/sherrygelato.jsx
@@ -0,0 +1,18 @@
+export default function Sherrygelato() {
+    return <>
+        <h1>hihihihihi</h1>
+    </>
+
+    // 1차 목표
+    // 레이아웃
+    // 상하 2등분
+    // 좌우 3등분
+
+
+    // 2차 목표 - 레이아웃 완료 후 해당 영역에 각 컴포넌트 넣어보기
+    // 소영 : input, draganddrop
+    // 이랑 : button, 
+    // 하늘 : dropdown
+    // 승은 : card, calender
+    // 영주 : draganddrop, dark/light mode
+}
