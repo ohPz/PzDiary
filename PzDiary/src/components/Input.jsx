9b581929c2d26106fcfed2ffc027b00357797cfe
laire7 <syoungkwak@gmail.com>
2024-06-20 13:41:06 +0900

    input1

--- PzDiary/src/components/Input.jsx
+++ PzDiary/src/components/Input.jsx
@@ -1,1 +1,7 @@
-Lala~
+export default function Input() {
+    return{
+        <form onsubmit="submitForm(event)">
+        <input type="text" id="inp" placeholder="I have to..." />
+        </form>
+    }
+}
