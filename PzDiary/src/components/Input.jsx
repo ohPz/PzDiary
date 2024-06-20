a34420c703c9c2c3560a3819d14e820ad7821702
laire7 <syoungkwak@gmail.com>
2024-06-20 14:06:03 +0900

    Input 추가

--- PzDiary/src/components/Input.jsx
+++ PzDiary/src/components/Input.jsx
@@ -1,7 +1,7 @@
 export default function Input() {
-    return{
-        <form onsubmit="submitForm(event)">
-        <input type="text" id="inp" placeholder="I have to..." />
-        </form>
-    }
+  return (
+    <form onsubmit="submitForm(event)">
+      <input type="text" id="inp" placeholder="I have to..." />
+    </form>
+  );
 }
