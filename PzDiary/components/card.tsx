38f2b6ed1669c74525d5a72e1567601380a8d415
sherrygelato <sherrygelato@kakao.com>
2024-07-02 14:59:47 +0900

    card jsx=>tsx로 업데이트

--- PzDiary/components/card.tsx
+++ PzDiary/components/card.tsx
@@ -1,11 +1,19 @@
-export default function Card(card) {
-  const memo = card.card;
+type Children = {
+  title: string;
+  detail: string;
+  btn: any;
+};
+
+export default function Card(children: Children) {
+  const { title, detail, btn } = children;
   return (
     <>
-      <div className="flex justify-center border-2 border-black m-4">
-        <div className="block max-w-[18rem] rounded-lg bg-success text-white shadow-secondary-1">
-          <h5 className="border-b-2 border-black/20"></h5>
-          <div className="p-6">{memo.title}</div>
+      <div className='flex justify-center border-2 border-black m-4'>
+        <div className='block max-w-[18rem] rounded-lg bg-success text-white shadow-secondary-1'>
+          <h5 className='border-b-2 border-black/20'></h5>
+          <div className='p-6'>{title}</div>
+          <button onClick={btn}>EDIT</button>
+          <div className='p-6'>{detail}</div>
         </div>
       </div>
     </>
