48b1519be9c601bc9223b8681d5f1f1e4a7d9ec0
young-droid <young.yj.cho@gmail.com>
2024-07-10 16:37:47 +0900

    login and out and profile img

--- PzDiary/container/yj.tsx
+++ PzDiary/container/yj.tsx
@@ -1,7 +1,14 @@
+import { SignIn } from '@/components/signin';
+import { SignOut } from '@/components/signout';
+import UserAvatar from '@/components/user-avatar';
+
 export default function YjContainer() {
   return (
     <>
       <h1>yj</h1>
+      <SignIn></SignIn>
+      <SignOut></SignOut>
+      <UserAvatar></UserAvatar>
     </>
   );
 }
