a470e74d8f8a573f1b3ca325740972696afa80a6
sherrygelato <sherrygelato@kakao.com>
2024-07-10 17:00:59 +0900

    삭제해야 할 파일 삭제

--- src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-const openWindowButton = document.getElementById('openWindowButton');
-
-openWindowButton.addEventListener('click', function () {
-  const newWindow = window.open('https://www.google.com', '_blank');
-}); //구글 새창으로 열기
