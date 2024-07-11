6dd4b67a5a799e94dcfb1cfd9c0e4e6f0a55174b
sherrygelato <sherrygelato@kakao.com>
2024-07-11 14:27:37 +0900

    layout 다시 나누기

--- PzDiary/app/page.tsx
+++ PzDiary/app/page.tsx
@@ -7,29 +7,28 @@ import YrContainer from '@/container/yr';
 export default function Home() {
   return (
     <div className='flex flex-col w-screen h-screen'>
-      <div className='flex flex-row h-1/2'>
-        <div className='flex-1 bg-blue-200'>
-          곽소영
-          <SyContainter />
-        </div>
+      <div className='flex flex-col h-1/2'>
         <div className='flex-1 bg-pink-200'>
           조영주
           <YjContainer />
         </div>
-      </div>
-
-      <div className='flex flex-row h-1/2'>
-        <div className='flex-1 bg-green-200'>
-          이승은
-          <SeContainer />
+        <div className='flex-1 bg-yellow-200'>
+          김하늘
+          <HnContainer />
         </div>
         <div className='flex-1 bg-red-200'>
           임이랑
           <YrContainer />
         </div>
-        <div className='flex-1 bg-yellow-200'>
-          김하늘
-          <HnContainer />
+      </div>
+      <div className='flex flex-row h-1/2'>
+        <div className='flex-1 bg-green-200'>
+          이승은
+          <SeContainer />
+        </div>
+        <div className='flex-1 bg-blue-200'>
+          곽소영
+          <SyContainter />
         </div>
       </div>
     </div>
