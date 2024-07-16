701c96ff4e976cecd7e4179f50a4a971f5b48df0
yirang07 <yirang07@gmail.com>
2024-07-16 11:03:48 +0900

    서치바 1차

--- PzDiary/container/yr.tsx
+++ PzDiary/container/yr.tsx
@@ -1,7 +1,28 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { MouseEvent, MouseEventHandler, useState } from 'react';
+
 export default function YrContainer() {
+  const [todoDetail, todeTitle] = useState<Todo[]>([]);
+  const [search, setSearch] = useState('');
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <>
-      <h1>yr</h1>
+      <input
+        type='text'
+        name='detail'
+        value={search}
+        onChange={onChange}
+        placeholder='할일찾기'
+      />
+
+      <Button onClick={onChange} type='button'>
+        search
+      </Button>
     </>
   );
 }
