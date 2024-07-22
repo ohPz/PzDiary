6d8564100e041abf2945359af311805dff22aa02
young-droid <young.yj.cho@gmail.com>
2024-07-22 08:48:50 +0200

    redirect to /my after signup

--- PzDiary/app/regist/page.tsx
+++ PzDiary/app/regist/page.tsx
@@ -1,25 +1,21 @@
 'use client';
 
 import { signIn, signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
+// import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function RegistPage() {
   const { data: session } = useSession();
 
   if (session) {
-    return (
-      <div>
-        <p>Signed in as {session.user?.email}</p>
-
-        <Link href='/'>Go back to Main</Link>
-        <button onClick={() => signOut()}>Sign out</button>
-      </div>
-    );
+    return redirect('/my');
   }
   return (
-    <>
+    <div className='flex flex-col items-center'>
       <h1>Register</h1>
-      <button onClick={() => signIn('google')}>Sign up with Google</button>
-    </>
+      <h1>
+        <button onClick={() => signIn('google')}>Sign up with Google</button>
+      </h1>
+    </div>
   );
 }
